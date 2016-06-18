763c56e0a002badb26438faa0d96d06a952e595b
Jeff Schnurr <jschnurr@gmail.com>
2016-06-18 04:10:45 -0400

    cleaned up content and formatting, adjusted line heights and font size, enabled tipue search

--- content/pages/about.md
+++
@@ -1,7 +0,0 @@
-Title: About Me
-Date: 2016-06-15 10:20
-Slug: about-me
-
-*About Me*
-
-Testing.
--- pelicanconf.py
+++ pelicanconf.py
@@ -5,12 +5,16 @@ from __future__ import unicode_literals
 AUTHOR = u'Jeff Schnurr'
 SITENAME = u'jeffsidea.com'
 SITEURL = ''
-# SITELOGO = 'images/jeffsidea-552x120-standard.png'
-# SITELOGO_SIZE = '552px'
+# SITELOGO = 'images/jeffsidea-01.png'
+# SITELOGO_SIZE = '40'
 BANNER = 'images/jeffsidea-02.png'
 BANNER_ALL_PAGES = False
 FAVICON = 'images/favicon.png'
 HIDE_SITENAME = False
+SUMMARY_MAX_LENGTH = 150
+
+# can be useful in development, but set to False when you're ready to publish
+RELATIVE_URLS = True
 
 PATH = 'content'
 
@@ -30,12 +34,18 @@ AUTHOR_FEED_RSS = None
 
 # Social widget
 SOCIAL = (('Twitter', 'http://www.twitter.com/jeffsidea'),
-          ('LinkedIn', 'https://ca.linkedin.com/in/schnurr'),)
+          ('LinkedIn', 'https://ca.linkedin.com/in/schnurr'),
+          ('GitHub', 'https://github.com/jschnurr'),
+          ('Facebook', 'https://www.facebook.com/jschnurr'),)
 
 DEFAULT_PAGINATION = 10
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
+
+DIRECT_TEMPLATES = ('index', 'categories', 'tags', 'archives', 'search')
+PLUGIN_PATHS = ['plugins']
+PLUGINS = ['tipue_search','tag_cloud']
 
 # Theme config
 THEME='theme'
@@ -53,13 +63,10 @@ ADDTHIS_PROFILE='ra-5761ffa09e14ce18'
 TWITTER_USERNAME='jeffsidea'
 TWITTER_WIDGET_ID='663435651132489728'
 
-PLUGIN_PATHS = ["plugins"]
-PLUGINS = ["tag_cloud"]
-
 TAG_CLOUD_STEPS = 4
 TAG_CLOUD_MAX_ITEMS = 100
 TAG_CLOUD_SORTING = 'random'
 TAG_CLOUD_BADGE = True
 
-ABOUT_ME = 'this is all about me.'
+ABOUT_ME = 'Innovation Lab Director in Waterloo. Ignites ideas through technology and design.'
 AVATAR = 'images/jschnurr-profile-300x200.jpg'
