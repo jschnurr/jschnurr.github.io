3c5e29601edbe1bdc2fae29576471d65eb8060db
Jeff Schnurr <jschnurr@gmail.com>
2016-06-19 15:13:40 -0400

    fix domain name

--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'www.jeffsidea.com'
+SITEURL = 'http://www.jeffsidea.com'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
